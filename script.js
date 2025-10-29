49e022c2d4b85cca153e8f1ac7f3b3de9a8d0ecc
lilo0406 <lic.matizlina@gmail.com>
2025-10-29 01:02:07 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const scriptURL = "TU_URL_DE_APPS_SCRIPT_AQUI";
+const scriptURL = "https://script.google.com/macros/s/AKfycbx9b0IWIYrU-luq3wjPKij6Q_ldUB8st0iW6CX37to1R1TPNzhoJHxeMwiT7KwE5dh0KA/exec";
 const sheetURL  = "https://docs.google.com/spreadsheets/d/1mGoGQXWjT3_fb2d271m8kXG8PfsLG3iD_ZLelYXWjf0/edit?gid=0#gid=0";
 
 const form = document.getElementById("boletaForm");
